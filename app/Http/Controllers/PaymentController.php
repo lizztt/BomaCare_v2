99f3cfa31212389f1fbe435000a8d92b1c2ebd77
Bill Gichuru Chakairu <115233741+lizztt@users.noreply.github.com>
2024-07-08 13:24:55 +0300

    Update Payment Module
    Created payments folder in resources/views to store the files on the payment module.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -10,7 +10,7 @@ class PaymentController extends Controller
 {
     public function showPaymentForm()
     {
-        return view('payments');
+        return view('payments.payments');
     }
 
     
